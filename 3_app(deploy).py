083cc527c3aa6e33fe2669e1e84c6f8516b13d22
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-02-05 23:38:54 +0530

    Update 3_app(deploy).py

--- 3_app(deploy).py
+++ 3_app(deploy).py
@@ -6,9 +6,11 @@ import pickle
 import streamlit as st
 
 
+
 model=tf.keras.models.load_model('model.h5')
 
-with open('onehot_encoder_geo.pkl','rb') as file:  
+
+with open('onehot_encoder_geo.pkl','rb') as file: 
     onehot_encoder_geo=pickle.load(file)
 
 with open('label_encoder_gender.pkl','rb') as file:
@@ -20,6 +22,7 @@ with open('scaler.pkl','rb') as file:
 
 st.title('Customer Churn Predection')
 
+
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
@@ -30,6 +33,7 @@ tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
+
 
 input_data = pd.DataFrame({  
     'CreditScore': [credit_score],
@@ -48,9 +52,12 @@ input_data = pd.DataFrame({
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
+
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
+
 input_data_scaled = scaler.transform(input_data)
+
 
 
 prediction = model.predict(input_data_scaled)
