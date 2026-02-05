eb925feb6cd68684d36b83233ba0a65e410786c3
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-02-05 23:41:04 +0530

    Update 3_app(deploy).py

--- 3_app(deploy).py
+++ 3_app(deploy).py
@@ -6,11 +6,15 @@ import pickle
 import streamlit as st
 
 
+#Without Keras, writing ANN code is long and complex.Keras automatically manages:Forward propagation,Backpropagation,Weight updates,Gradient calculation,Optimization
 
+
+#load the trained models
 model=tf.keras.models.load_model('model.h5')
 
 
-with open('onehot_encoder_geo.pkl','rb') as file: 
+##load the encoder and scalar 
+with open('onehot_encoder_geo.pkl','rb') as file:  #rb-read binary
     onehot_encoder_geo=pickle.load(file)
 
 with open('label_encoder_gender.pkl','rb') as file:
@@ -20,9 +24,11 @@ with open('scaler.pkl','rb') as file:
     scaler=pickle.load(file)
 
 
+#streamlit app
 st.title('Customer Churn Predection')
 
-
+# for getting the detail of user input how its working go on-->"https://chatgpt.com/c/6984d321-1dfc-8322-b43e-426052e75b95"
+# User input(just like a form)
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
@@ -34,10 +40,10 @@ num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-
-input_data = pd.DataFrame({  
+# Prepare the input data
+input_data = pd.DataFrame({  ##Pandas expects columns as lists that's why we are using xyz=[xyz]
     'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]], 
+    'Gender': [label_encoder_gender.transform([gender])[0]], # ML model cannot understand text, Gender was Label Encoded during training
     'Age': [age],
     'Tenure': [tenure],
     'Balance': [balance],
@@ -49,17 +55,18 @@ input_data = pd.DataFrame({
 
 
 
+# One-hot encode for 'Geography'
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
-
+# Combine one-hot encoded columns with input data   -->(Germany,spain and france will be added after this concatenation with input_data in original data)
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-
+# Scale the input data  -->->data will converted into array
 input_data_scaled = scaler.transform(input_data)
 
 
-
+#for predection
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
